f3c425fee86cbdc195da4622c524f53a651f56e2
Nimira43 <n.rabone72@aol.com>
2025-01-19 09:06:28 +0000

    Animations reviewed

---
+++ 05-Animations/src/main.js
@@ -0,0 +1,42 @@
+import * as THREE from 'three'
+import './main.css'
+import gsap from 'gsap'
+
+const canvas = document.querySelector('.canvas')
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff4500 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+const sizes = {
+    width: 1100,
+    height: 650
+}
+
+const camera = new THREE.PerspectiveCamera(
+  75, 
+  sizes.width / sizes.height
+)
+camera.position.z = 3.5
+scene.add(camera)
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+gsap.to(mesh.position, { 
+  duration: 1, 
+  delay: 1.5, 
+  x: 2,
+  y:2
+})
+
+const tick = () => {
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(tick)
+}
+
+tick()
